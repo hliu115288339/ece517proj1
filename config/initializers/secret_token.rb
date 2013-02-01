3584815afa98cd35bc2c22011a85323ed527d63f
Hao Liu <hliu11@ncsu.edu>
2013-02-01 09:35:59 -0500

    first commit: freshly generated rail application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ece517proj1::Application.config.secret_token = '9bbf35bb7b2b0da2a9a7ff967a88c1b135e017d6d06fddc590a97264687126800f3e003d220a0e254e558daf843a9380d9f6688780ccccdfd39f160d87af8cd6'
